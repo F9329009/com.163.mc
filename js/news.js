3dbcd1ac96fc610857f3f82bb52cc4627216b83f
F9329009 <9329009@qq.com>
2020-05-04 21:36:32 +0800

    更新

---
+++ js/news.js
@@ -0,0 +1,405 @@
+// 顶部导航-广告
+$(".header-ad").hover(
+    function () {
+        $(this).css({ overflow: "visible" }).find(">img").hide();
+    },
+    function () {
+        console.log(2222);
+        $(this).css({ overflow: "hidden" }).find(">img").show();
+    }
+);
+
+// 二维码绿线上下移动
+function qrcodeDiv() {
+    $(".qrcode div").animate(
+        {
+            top: -10,
+        },
+        2000,
+        function () {
+            $(this).animate(
+                {
+                    top: $(this).siblings("img").height() - 10,
+                },
+                2000
+            );
+        }
+    );
+}
+// 先执行一次(解决定时器4秒后才能执行第一次)
+qrcodeDiv();
+// 设置定时器循环执行
+setInterval(qrcodeDiv, 4000);
+
+// 顶部导航-游戏目录显示隐藏
+$(".topbar-left").hover(
+    function () {
+        $(".topbar-game").css({
+            height: 500,
+        });
+    },
+    function () {
+        $(".topbar-game").css({
+            height: 0,
+        });
+    }
+);
+
+$(".topbar-game").hover(
+    function () {
+        $(".topbar-game").css({
+            height: 500,
+        });
+    },
+    function () {
+        $(".topbar-game").css({
+            height: 0,
+        });
+    }
+);
+
+// 顶部导航-游戏目录小图标
+let menuListArr = [
+    "NO1",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    ,
+    ,
+    ,
+    "HOT",
+    "HOT",
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    ,
+    ,
+    ,
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    "HOT",
+    ,
+    ,
+    ,
+    ,
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    "NEW",
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    "HOT",
+    "HOT",
+    "HOT",
+    "NEW",
+    "NEW",
+    ,
+    ,
+    "NEW",
+    "HOT",
+];
+menuListArr.forEach(function (m, i) {
+    if (m == "NO1") {
+        $(".topbar-game-list").find("em").eq(i).css({
+            background: "url(./images/sprite-w_dd519f4.png) no-repeat",
+            backgroundPosition: "right -267px",
+        });
+    } else if (m == "HOT") {
+        $(".topbar-game-list").find("em").eq(i).css({
+            background: "url(./images/sprite-w_dd519f4.png) no-repeat",
+            backgroundPosition: "right -144px",
+        });
+    } else if (m == "NEW") {
+        $(".topbar-game-list").find("em").eq(i).css({
+            background: "url(./images/sprite-w_dd519f4.png) no-repeat",
+            backgroundPosition: "right -206px",
+        });
+    }
+});
+
+// 顶部导航-右边轮播
+let topbarRightArr = ["25元专属券限时领！", "限时领取免费教程"];
+
+setInterval(function () {
+    $(".topbar-right a").animate(
+        {
+            opacity: 0,
+            top: "-100%",
+        },
+        400,
+        function () {
+            let index = topbarRightArr.indexOf($(".topbar-right a").text());
+            if (index < topbarRightArr.length - 1) {
+                $(".topbar-right a").text(topbarRightArr[index + 1]);
+            } else {
+                $(".topbar-right a").text(topbarRightArr[0]);
+            }
+            $(this)
+                .css({
+                    top: "100%",
+                })
+                .animate(
+                    {
+                        opacity: 1,
+                        top: 0,
+                    },
+                    400
+                );
+        }
+    );
+}, 4000);
+
+// 顶部导轨-热点推荐
+$(".topbar-recommend").hover(
+    function () {
+        $(".topbar-hot").css({
+            height: 378,
+        });
+    },
+    function () {
+        $(".topbar-hot").css({
+            height: 0,
+        });
+    }
+);
+
+$(".topbar-hot").hover(
+    function () {
+        $(".topbar-hot").css({
+            height: 378,
+        });
+    },
+    function () {
+        $(".topbar-hot").css({
+            height: 0,
+        });
+    }
+);
+
+// 浮动导航-固定顶部
+function getFloatnav() {
+    if ($(document).scrollTop() >= $(".topbar").height()) {
+        $(".floatnav")
+            .css({
+                position: "fixed",
+                top: 0,
+                height: 118,
+                background:
+                    "url(./images/top_bg_3cd9741.png) center top no-repeat",
+            })
+            .find(".header-logo")
+            .hide()
+            .siblings(".floatnav-left")
+            .show();
+    } else {
+        $(".floatnav")
+            .css({
+                position: "absolute",
+                top: 55,
+                height: 92,
+                background:
+                    "url(./images/top_bg1_db2a296.png) center top no-repeat",
+            })
+            .find(".header-logo")
+            .show()
+            .siblings(".floatnav-left")
+            .hide();
+    }
+}
+getFloatnav();
+$(document).scroll(getFloatnav);
+
+// 右侧浮动打开
+$(".side-show").on("click", function () {
+    $(this).css("right", -50);
+    $(".side").css("right", 0);
+});
+// 右侧浮动关闭
+$(".close").on("click", function () {
+    $(".side").css("right", -$(".side").width() - $(".close").width());
+    $(".side-show").css("right", 0);
+});
+// 默认自动打开
+$(".side-show").click();
+
+// 右边浮动-轮播图
+$(".act-box").css({ left: 0 });
+function getCarousel() {
+    // 判断轮播图是否到最后一张
+    if (
+        $(".act-box").position().left * -1 >=
+        $(".act-box a").width() * ($(".act-box a").length - 1)
+    ) {
+        $(".act-box").css({ left: 0 });
+    }
+    console.log(
+        $(".act-box").position().left - $(".act-box a").width(),
+        $(".act-box a").width()
+    );
+    $(".act-box").animate(
+        {
+            left: $(".act-box").position().left - $(".act-box a").width(),
+        },
+        200
+    );
+    // 小点
+    $(".act-dot span")
+        .removeClass("active")
+        .eq(($(".act-box").position().left * -1) / $(".act-box a").width())
+        .addClass("active");
+}
+// 克隆最后一张轮播图到最前面
+$(".act-box")
+    .prepend($(".act-box a:last").clone(true))
+    .css({ left: -$(".act-box a").width() });
+// 实时计算轮播图宽度
+$(".act-box").css({
+    width: $(".act-box a").length * $(".act-box a").width(),
+});
+// 轮播
+carouselTimer = setInterval(getCarousel, 2000);
+// 鼠标移入暂停轮播
+$(".act-box").hover(
+    function () {
+        clearInterval(carouselTimer);
+    },
+    function () {
+        carouselTimer = setInterval(getCarousel, 2000);
+    }
+);
+// 鼠标移入小点切换轮播图
+$(".act-dot").on("mousemove", "span", function () {
+    let $index = $(this).index();
+    // 暂停轮播
+    clearInterval(carouselTimer);
+    // 图片切换
+    $(".act-box")
+        .stop(true)
+        .animate(
+            {
+                left: -$(".act-box a").width() * ($(this).index() + 1),
+            },
+            200,
+            function () {
+                // 小点切换
+                $(".act-dot span")
+                    .removeClass("active")
+                    .eq($index)
+                    .addClass("active");
+            }
+        );
+    // 启动轮播
+    carouselTimer = setInterval(getCarousel, 2000);
+});
+
+// 二维码切换
+$(".ewm-tab").on("mouseover", "a", function () {
+    $(this).addClass("active").siblings("a").removeClass("active");
+    $(".ewm-img").animate(
+        {
+            left: -$(".ewm-img>div").width() * $(this).index(),
+        },
+        400
+    );
+});
